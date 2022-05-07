d4247403568bc281994b46217a4446c5a72a2d20
Precious Okon <50714079+jaeleen@users.noreply.github.com>
2022-05-06 21:49:47 -0600

    Add file via upload

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-#Data Types
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
 
-#string
+#Write your code below this line 👇
+new_age = int(age)
+remaining_age = 90 - new_age
+months_left = remaining_age * 12
 
-print ("Hello"[4])
-#returns H
+weeks_left = remaining_age * 52
+days_left = remaining_age * 365
 
-print("hello" + "345")
-#returns hello345
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
 
-#Integer
 
-print(123 + 345)
-#returns 468
 
-# 123_456_789 - 123,456,789
-
-num_char = len(input("What is your name?"))
-
-new_num_char = str(num_char)
-
-print("Your name has " + new_num_char + " charactes.")
-
-3 + 5
-7 - 4
-4 * 4
-print(6/3)
-print(2**2)
-
-print(4/2)
-score = 0
-height = 1.8
-isWinning = True
-#f-String
-print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
