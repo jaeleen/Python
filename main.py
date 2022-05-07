b01b109eca4f4102ff1115178edcef34f80ca25d
Precious Okon <50714079+jaeleen@users.noreply.github.com>
2022-05-06 21:47:33 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+#Data Types
+
+#string
+
+print ("Hello"[4])
+#returns H
+
+print("hello" + "345")
+#returns hello345
+
+#Integer
+
+print(123 + 345)
+#returns 468
+
+# 123_456_789 - 123,456,789
+
+num_char = len(input("What is your name?"))
+
+new_num_char = str(num_char)
+
+print("Your name has " + new_num_char + " charactes.")
+
+3 + 5
+7 - 4
+4 * 4
+print(6/3)
+print(2**2)
+
+print(4/2)
+score = 0
+height = 1.8
+isWinning = True
+#f-String
+print(f"Your score is {score}, your height is {height}, you are winning is {isWinning}")
